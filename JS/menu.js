4ecdb24a05df0a3f01d13ad3597610035fb81cca
zhaysuser <88466798+zhaysuser@users.noreply.github.com>
2021-08-04 20:11:11 -0500

    Add files via upload

---
+++ JS/menu.js
@@ -0,0 +1,24 @@
+const hamburguer = document.querySelector('.hamburguesa')
+const menu = document.querySelector('.menu-navegacion')
+
+console.log(menu)
+console.log(hamburguer)
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && e.target != menu && e.target != hamburguer    ){
+        
+        
+        menu.classList.toggle("spread")
+    }
+})
+
+
+
+
+
+
+    
